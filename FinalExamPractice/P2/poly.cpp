c1bb2b4d79bd8c17279beaec32d6e13aa6db5bb4
Kevin Kuriachan <kevinkuriachan@compute.cs.tamu.edu>
2018-05-01 15:49:13 -0500

    added .gitignore to remove *.o files

---
+++ FinalExamPractice/P2/poly.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+
+#include "poly.h"
+
---
+++ FinalExamPractice/P2/poly.h
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#ifndef __POLY__
+#define __POLY__
+
+class poly {
+
+	public:
+
+
+	private:
+};
+
+
+#endif 
